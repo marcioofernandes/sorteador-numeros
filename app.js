08ff83752a6cef875d86f200147a4f43a9929715
projetoenquadro <marc.fernandes13@gmail.com>
2024-08-01 23:24:09 -0300

    Código finalizado

--- app.js
+++ app.js
@@ -11,13 +11,40 @@ function sortear () {
 // Esse é um loop for, nem aprendi sobre ele nas aulas, pesquisar sobre
   for (let i = 0; i < quantidade; i++) {
     numero = obterNumeroAleatório(de, ate);
+
+    while (sorteados.includes(numero)) {
+      numero = obterNumeroAleatório (de, ate);
+    }
+
     sorteados.push(numero);
   }
-
+//trecho do código onde aparece os numeros sorteados na tela
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`;
+  alterarStatusBotao ();
 };
 
 function obterNumeroAleatório (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 
 }
+//funcão para habilitar e desabilitar botões 
+function alterarStatusBotao () {
+  let botao = document.getElementById("btn-reiniciar");
+  if (botao.classList.contains(`container__botao-desabilitado`)) {
+    botao.classList.remove("container__botao-desabilitado");
+    botao.classList.add("container__botao");
+  } else {
+    botao.classList.remove("container_botao")
+    botao.classList.add("container__botao-desabilitado");
 
+  }
+}
+// ativando botão reiniciar"
+function reiniciar (){
+  document.getElementById("quantidade").value = ''; 
+  document.getElementById("de").value = ''; 
+  document.getElementById("ate").value = ''; 
+  document.getElementById("resultado").innerHTML = 'Números sorteados:  nenhum até agora<'; 
+  alterarStatusBotao(); 
+}
